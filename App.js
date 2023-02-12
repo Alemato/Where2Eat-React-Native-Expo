75d1dd061ae8edebd4ea463e114da3db58d2a49a
Stefano Decina <stefano.decina@student.univaq.it>
2023-02-12 17:32:01 +0100

    costruzione in progress card ristorante

--- App.js
+++ App.js
@@ -1,52 +1,57 @@
 import {FlatList, StyleSheet, View} from 'react-native';
-import RestaurantCard from "./src/components/RestaurantCard";
+import RestaurantCard from './src/components/RestaurantCard';
 
 export default function App() {
 
-    const ristoranti = [
-        {
-            id: '1',
-            ragioneSociale: 'Villa Crespi 1',
-            descrizione: 'ababablue blue'
-        },
-        {
-            id: '2',
-            ragioneSociale: 'Villa Crespi 2',
-            descrizione: 'ababablue blue'
-        },
-        {
-            id: '3',
-            ragioneSociale: 'Villa Crespi 3',
-            descrizione: 'ababablue blue'
-        },
-        {
-            id: '4',
-            ragioneSociale: 'Villa Crespi 4',
-            descrizione: 'ababablue blue'
-        },
-    ];
+  const ristoranti = [
+    {
+      id: '1',
+      ragioneSociale: 'Villa Crespi 1',
+      descrizione: 'ababablue blue',
+      voto: '10',
+    },
+    {
+      id: '2',
+      ragioneSociale: 'Villa Crespi 2',
+      descrizione: 'ababablue blue',
+      voto: '10',
+    },
+    {
+      id: '3',
+      ragioneSociale: 'Villa Crespi 3',
+      descrizione: 'ababablue blue',
+      voto: '10',
+    },
+    {
+      id: '4',
+      ragioneSociale: 'Villa Crespi 4',
+      descrizione: 'ababablue blue',
+      voto: '10',
+    },
+  ];
 
-    return (
-        <View style={styles.container}>
-            <FlatList style={styles.list}
-                      data={ristoranti}
-                      renderItem={({item}) => <RestaurantCard restaurantCard={{
-                          ragioneSociale: item.ragioneSociale,
-                          descrizione: item.descrizione,
-                      }}/>}
-                      keyExtractor={(item) => item.id}
-            />
-        </View>
-    );
+  return (
+      <View style={styles.container}>
+        <FlatList style={styles.list}
+                  data={ristoranti}
+                  renderItem={({item}) => <RestaurantCard restaurantCard={{
+                    ragioneSociale: item.ragioneSociale,
+                    descrizione: item.descrizione,
+                    voto: item.voto,
+                  }}/>}
+                  keyExtractor={(item) => item.id}
+        />
+      </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        margin: 10,
-        alignItems: 'center',
-    },
-    list: {
-        width: "100%",
-    }
+  container: {
+    flex: 1,
+    margin: 10,
+    alignItems: 'center',
+  },
+  list: {
+    width: '100%',
+  },
 });
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,82 +1,89 @@
 import React from 'react';
-import {Alert, Button, Image, StyleSheet, Text, View} from 'react-native';
+import {Button, Image, StyleSheet, Text, View} from 'react-native';
+import ItemContainer from './ItemContainer';
+import Card from './Card';
 
 class RestaurantCard extends React.Component {
 
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        const {restaurantCard} = this.props;
-        return (
-            <View style={styles.cardContainer}>
-                <View style={styles.card}>
-                    <Image style={styles.image}
-                           source={{uri: 'https://images.pexels.com/photos/260922/pexels-photo-260922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'}}
-                    />
-                    <View style={styles.texts}>
-                        <Text style={styles.ragioneSociale}>
-                            {restaurantCard.ragioneSociale}
-                        </Text>
-                        <Text>
-                            {restaurantCard.descrizione}
-                        </Text>
+  render() {
+    const {restaurantCard} = this.props;
 
-                        <View style={styles.buttonContent}>
-                            <Button style={styles.button}
-                                    title="Dettagli"
-                                    onPress={() => Alert.alert('Pagina ristorante')}
-                            />
-                        </View>
+    return (
+        <ItemContainer style={styles.cardContainer}>
+          <Card>
+            <Image style={styles.image}
+                   source={{uri: 'https://images.pexels.com/photos/260922/pexels-photo-260922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'}}
+            />
+            <View style={styles.texts}>
+              <View style={styles.titleRow}>
+                <Text style={styles.ragioneSociale}>
+                  {restaurantCard.ragioneSociale}
+                </Text>
+                <Text>
+                  {restaurantCard.voto}
+                </Text>
+              </View>
+              <Text>
+                {restaurantCard.descrizione}
+              </Text>
 
-                    </View>
-
-                </View>
+              <View style={styles.buttonContent}>
+                <Button title="Dettagli"
+                        onPress={() => {
+                          navigation.navigate('Ristorante');
+                        }}
+                />
+              </View>
             </View>
-        );
-    }
+          </Card>
+        </ItemContainer>
+    );
+  }
 }
 
 export default RestaurantCard;
 
 const styles = StyleSheet.create({
-    card: {
-        marginTop: 20,
-        shadowColor: 'black',
-        shadowOffset: {width: 0, height: 2},
-        shadowRadius: 6,
-        shadowOpacity: 0.26,
-        elevation: 8,
-        backgroundColor: 'white',
-        borderRadius: 10,
-        width: '100%',
-        // justifyContent: 'center', //Centered vertically
-        // alignItems: 'center', // Centered horizontally
-    },
-    ragioneSociale: {
-        fontSize: 18,
-        fontWeight: "bold",
-        marginBottom: 30
-    },
-    buttonContent: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        marginTop: 30
-    },
-    image: {
-        width: '100%',
-        height: 150,
-        borderTopLeftRadius: 10,
-        borderTopRightRadius: 10,
-    },
-    botton: {
-        borderRadius: 10
-    },
-    texts: {
-        padding: 20
-    },
-    cardContainer: {
-        marginHorizontal: 10
-    }
-})
+  card: {
+    marginTop: 20,
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    elevation: 8,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    width: '100%',
+  },
+  ragioneSociale: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 30,
+  },
+  buttonContent: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 30,
+  },
+  image: {
+    resizeMode: 'contain',
+    width: '100%',
+    height: 200,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+  },
+  texts: {
+    padding: 20,
+  },
+  cardContainer: {
+    marginHorizontal: 10,
+  },
+  titleRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
