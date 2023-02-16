4f33636a64d5e438c3cc384dc4526b3b68d75efe
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2023-02-16 10:52:37 +0100

    HomePage

--- App.js
+++ App.js
@@ -1,57 +1,22 @@
+/*
 import {FlatList, StyleSheet, View} from 'react-native';
+
 import RestaurantCard from './src/components/RestaurantCard';
+import {Provider} from 'react-redux';
+
+import {store, persistor} from './src/stores';
+import {PersistGate} from 'redux-persist/integration/react';
+*/
+import HomePageScreen from './src/pages/HomePageScreen';
 
 export default function App() {
-
-  const ristoranti = [
-    {
-      id: '1',
-      ragioneSociale: 'Villa Crespi 1',
-      descrizione: 'ababablue blue',
-      voto: '10',
-    },
-    {
-      id: '2',
-      ragioneSociale: 'Villa Crespi 2',
-      descrizione: 'ababablue blue',
-      voto: '10',
-    },
-    {
-      id: '3',
-      ragioneSociale: 'Villa Crespi 3',
-      descrizione: 'ababablue blue',
-      voto: '10',
-    },
-    {
-      id: '4',
-      ragioneSociale: 'Villa Crespi 4',
-      descrizione: 'ababablue blue',
-      voto: '10',
-    },
-  ];
-
   return (
-      <View style={styles.container}>
-        <FlatList style={styles.list}
-                  data={ristoranti}
-                  renderItem={({item}) => <RestaurantCard restaurantCard={{
-                    ragioneSociale: item.ragioneSociale,
-                    descrizione: item.descrizione,
-                    voto: item.voto,
-                  }}/>}
-                  keyExtractor={(item) => item.id}
-        />
-      </View>
+      <>
+      {/*<Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>*/}
+          <HomePageScreen />
+        {/*</PersistGate>
+      </Provider>*/}
+      </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    margin: 10,
-    alignItems: 'center',
-  },
-  list: {
-    width: '100%',
-  },
-});
---
+++ src/pages/HomePageScreen.js
@@ -0,0 +1,56 @@
+import {FlatList, StyleSheet, View} from 'react-native';
+import RestaurantCard from '../components/RestaurantCard';
+
+export default function HomePageScreen() {
+  const ristoranti = [
+    {
+      id: '1',
+      ragioneSociale: 'Villa Crespi 1',
+      descrizione: 'ababablue blue',
+      voto: '10',
+    },
+    {
+      id: '2',
+      ragioneSociale: 'Villa Crespi 2',
+      descrizione: 'ababablue blue',
+      voto: '10',
+    },
+    {
+      id: '3',
+      ragioneSociale: 'Villa Crespi 3',
+      descrizione: 'ababablue blue',
+      voto: '10',
+    },
+    {
+      id: '4',
+      ragioneSociale: 'Villa Crespi 4',
+      descrizione: 'ababablue blue',
+      voto: '10',
+    },
+  ];
+
+  return (
+      <View style={styles.container}>
+        <FlatList style={styles.list}
+                  data={ristoranti}
+                  renderItem={({item}) => <RestaurantCard restaurantCard={{
+                    ragioneSociale: item.ragioneSociale,
+                    descrizione: item.descrizione,
+                    voto: item.voto,
+                  }}/>}
+                  keyExtractor={(item) => item.id}
+        />
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 10,
+    alignItems: 'center',
+  },
+  list: {
+    width: '100%',
+  },
+});
