f1e8e2f88f299e02f7eda9f770d8a9e1d6f00ed5
Alessandro Mattei <alessandro.mattei1@student.univaq.it>
2023-02-21 08:06:03 +0100

    api

--- src/api/Api.js
+++ src/api/Api.js
@@ -5,9 +5,13 @@ export const API_URL = `http://${LOCALHOST}:8080/api/`;
 export const IMAGE_API_URL = `http://${LOCALHOST}:8080/uploads/`;
 export const TOKEN_JWT_NAME = 'access_token';
 
-function serializerResponse(response) {
+export function serializerResponse(response) {
+  let data;
+  if (Array.isArray(response.data)) {
+    data = [...response.data];
+  } else data = {...response.data};
   return {
-    data: response.data,
+    data: data,
     headers: {...response.headers},
     status: response.status,
     statusText: response.statusText,
@@ -28,6 +32,38 @@ class Api {
       telefono: telefono,
       password: password,
     }).then((response) => serializerResponse(response));
+  }
+
+  static modificaAccount({
+                           nome,
+                           cognome,
+                           telefono,
+                           email,
+                           statoAccount,
+                           ruolo,
+                           token,
+                         }) {
+    return axios.patch(API_URL + 'account', {
+      nome: nome,
+      cognome: cognome,
+      telefono: telefono,
+      email: email,
+      statoAccount: statoAccount,
+      ruolo: ruolo,
+    }, {headers: {Authorization: `Bearer ${token}`}}).
+        then((response) => serializerResponse(response));
+  }
+
+  static getHome({token}) {
+    return axios.get(API_URL + 'ristoranti',
+        {headers: {Authorization: `Bearer ${token}`}, params: {view: 'home'}}).
+        then((response) => serializerResponse(response));
+  }
+
+  static getPrenotazioni({token}) {
+    return axios.get(API_URL + 'prenotazioni',
+        {headers: {Authorization: `Bearer ${token}`}}).
+        then((response) => serializerResponse(response));
   }
 }
 
